6bea45fee5f366caf092e731e7ae4bc80a60ae2d
GSSoft Nepal <gssoftnepal@gmail.com>
2022-11-08 23:16:38 +0545

    setup tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel</title>
 
+        @vite('resources/css/app.css')
+
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 

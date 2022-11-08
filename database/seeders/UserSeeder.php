218684ebeca2604583d376d5d58cca441d94ece4
GSSoft Nepal <gssoftnepal@gmail.com>
2022-11-08 23:38:09 +0545

    created UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ["email" => "admin@example.com"],
+            [
+                "name" => "developer",
+                "email" => "admin@example.com",
+                "email_verified_at" => now(),
+                "password" => bcrypt("password"),
+                "remember_token" => Str::random(30),
+            ]
+        );
+
+        User::factory()
+            ->count(50)
+            ->create();
+    }
+}
